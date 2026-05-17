b070a146ee2f9585802c26b782bb122e35dafa0c
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-05-17 20:04:00 +0200

    Added wrap_address.bf test case

---
+++ src-bf/test/wrap_address.bf
@@ -0,0 +1,24 @@
+prints: Success!
+shift 6 to the left; wrapping to the end region of the buffer
+start reverse writing every character to the buffer
+splitting the characters like this: !ss|eccuS
+then print every character by shifting left until 0
+
+<<<<<<
+!: 33   +++[>+++++ +++++ +<-]>
+>
+s: 115  +++++ +++++[>+++++ +++++ +<-]>+++++
+>
+s: 115  +++++ +++++[>+++++ +++++ +<-]>+++++
+>
+e: 101  +++++ +++++[>+++++ +++++<-]>+
+>
+c: 99   +++++ +++++[>+++++ +++++<-]>-
+>
+c: 99   +++++ +++++[>+++++ +++++<-]>-
+>
+u: 117  +++++ +++++[>+++++ +++++ +<-]>+++++ ++
+>
+S: 83   +++++ +++++[>+++++ +++<-]>+++
+print until char is 0 (counter; char=0)
+[.<<]
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main()
     use bnemit::BNEmitterFactoryDefault;
 
     #[cfg(not(feature = "args"))]
-    let config: Config = Config { file_path: "src-bf\\test\\wrap_pos.bf".into() };
+    let config: Config = Config { file_path: "src-bf\\test\\wrap_address.bf".into() };
 
     #[cfg(feature = "args")]
     let config = {
@@ -31,10 +31,10 @@ fn main()
     let target_ctx = TargetContext { 
         os:     TargetOS::Linux, 
         arch:   Architecture::X86_64, 
-        format: OutputFormat::Assembly(AsmFlavor::NASM),
-        dest:   OutputDest::File(Some("output\\out.asm".into()))
-        // format: OutputFormat::Interpreted,
-        // dest:   OutputDest::Stdout
+        // format: OutputFormat::Assembly(AsmFlavor::NASM),
+        // dest:   OutputDest::File(Some("output\\out.asm".into()))
+        format: OutputFormat::Interpreted,
+        dest:   OutputDest::Stdout
     };
 
     bngen::generate_output::<BNEmitterFactoryDefault>(flat_instr_tree, target_ctx);
