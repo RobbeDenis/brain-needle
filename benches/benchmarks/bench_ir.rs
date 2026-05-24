18540be3a17b8b7b5042dd78d6b4a058d3775ff6
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-05-25 00:33:10 +0200

    Refactor benches - removed mandelbrot_max.bf and now just repeat the extreme version

---
+++ benches/benchmarks/bench_ir.rs
@@ -0,0 +1,21 @@
+use criterion::*;
+use std::hint::black_box;
+use brain_needle::bnintrep::generate_intermediate_representation;
+
+pub fn generate_ir(c: &mut Criterion)
+{
+    let contents = std::fs::read_to_string("src-bf/mandelbrot_extreme.bf").unwrap();
+    let heavy_load = contents.repeat(8).into_bytes();
+
+    c.bench_function("Generate IR", |b| {
+        b.iter_with_setup(|| {
+                std::io::Cursor::new(heavy_load.clone())
+            },
+            |reader| {
+                generate_intermediate_representation(black_box(reader))
+            }
+        )
+    });
+}
+
+criterion_group!(benches, generate_ir);
