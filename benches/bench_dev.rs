44d19469de3819d909a59e1a8e4a38874e63f80b
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-05-24 13:14:22 +0200

    Clean up bench

--- benches/bench_dev.rs
+++ benches/bench_dev.rs
@@ -5,21 +5,21 @@ use brain_needle::bnintrep::generate_intermediate_representation;
 use brain_needle::bnlex::tokenize_file_from_path;
 use brain_needle::bnparse::create_flat_instr_tree_from_tokens;
 
-pub fn compare_generate_ir_all(c: &mut Criterion) 
+pub fn compare_generate_ir(c: &mut Criterion) 
 {
-    let mut group = c.benchmark_group("Generate IR: mandelbrot");
+    let mut group = c.benchmark_group("Generate IR: compare");
 
     group.sample_size(200);
     group.measurement_time(std::time::Duration::new(20, 0));
 
     for id in ["base", "extreme", "max"] {
         let setup_reader = || bncore::create_bnreader(format!("src-bf\\mandelbrot_{}.bf", id));
-        group.bench_function(BenchmarkId::new("Old", id), |b| {
+        group.bench_function(BenchmarkId::new(id, "Old"), |b| {
             b.iter_with_setup(setup_reader, |reader| {
                 create_flat_instr_tree_from_tokens(tokenize_file_from_path(black_box(reader)).unwrap())
             })
         });
-        group.bench_function(BenchmarkId::new("New", id), |b| {
+        group.bench_function(BenchmarkId::new(id, "New"), |b| {
             b.iter_with_setup(setup_reader, |reader| {
                 generate_intermediate_representation(black_box(reader))
             })
@@ -28,36 +28,6 @@ pub fn compare_generate_ir_all(c: &mut Criterion)
 
     group.finish();
 }
-
-pub fn compare_generate_ir_max(c: &mut Criterion)
-{
-    let file_path = std::path::PathBuf::from("src-bf\\mandelbrot_max.bf");
-    let mut group = c.benchmark_group("Generate IR: mandelbrot_max.bf");
-    group.sample_size(200);
-    group.measurement_time(std::time::Duration::new(20, 0));
-
-    group.bench_function("Old", |b| {
-        b.iter_with_setup(|| {
-                bncore::create_bnreader(&file_path)
-            },
-            |reader| {
-                create_flat_instr_tree_from_tokens(tokenize_file_from_path(reader).unwrap())
-            }
-        )
-    });
-
-    group.bench_function("New", |b| {
-        b.iter_with_setup(|| {
-                bncore::create_bnreader(&file_path)
-            },
-            |reader| {
-                generate_intermediate_representation(black_box(reader))
-            }
-        )
-    });
-
-    group.finish();
-} 
 
 pub fn generate_ir(c: &mut Criterion)
 {
@@ -75,6 +45,5 @@ pub fn generate_ir(c: &mut Criterion)
 } 
 
 criterion_group!(benches, generate_ir);
-// criterion_group!(benches, compare_generate_ir_max);
-// criterion_group!(benches, compare_generate_ir_all);
+// criterion_group!(benches, compare_generate_ir);
 criterion_main!(benches);
