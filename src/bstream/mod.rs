730316221169517966c358e62db33d2ec1b18ffb
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2026-06-04 22:47:11 +0200

    WIP added alloc_or_increment functionality

--- src/bstream/mod.rs
+++ src/bstream/mod.rs
@@ -1,5 +1,6 @@
 use std::marker::PhantomData;
 use std::cell::Cell;
+use std::vec::Vec;
 use crate::bnintermediate::*;
 use crate::bnerror::BNError;
 
@@ -121,6 +122,13 @@ pub struct IndexId<'id> {
     _id: Id<'id>,
 }
 
+#[derive(Copy, Clone)]
+pub struct TypedIndexId<'id> {
+    index: usize,
+    typedId: u8,
+    _id: Id<'id>,
+}
+
 pub struct ByteStream {
     ctx: ByteStreamCtx<'static>,
 }
@@ -142,24 +150,40 @@ impl<'id> ByteStreamCtx<'id> {
     #[inline]
     fn new() -> Self {
         return Self { 
-            stream: std::vec::Vec::new(),
+            stream: Vec::new(),
             _id: PhantomData,
         };
     }
 
-    fn alloc<IValue: Instruction>(&mut self) -> IndexId<'id> {
+    fn alloc<IValue: Instruction>(&mut self) -> TypedIndexId<'id> {
         let offset: usize = self.stream.len();
         self.stream.push(IValue::ID);
 
-        return IndexId { index: offset, _id: PhantomData }
+        return TypedIndexId { index: offset, typedId: IValue::ID, _id: PhantomData }
     }
 
-    fn alloc_packed<Packer: InstructionPacker>(&mut self, value: Packer::IValue) -> IndexId<'id> {
+    fn alloc_packed<Packer: InstructionPacker>(&mut self, value: Packer::IValue) -> TypedIndexId<'id> {
         let offset: usize = self.stream.len();
         self.stream.push(Packer::ID);
         Packer::pack_value(&mut self.stream, value);
 
-        return IndexId { index: offset, _id: PhantomData }
+        return TypedIndexId { index: offset, typedId: Packer::ID, _id: PhantomData }
+    }
+
+    fn alloc_or_increment<Packer: InstructionPacker>(&mut self, id: TypedIndexId<'id>) -> TypedIndexId<'id> 
+        where Packer::IValue: std::ops::Add<Output = Packer::IValue> + From<u8> {
+        if id.typedId == Packer::ID {
+            unsafe {
+                let ptr = self.stream.as_mut_ptr().add(id.index + 1);
+                ptr.cast::<Packer::IValue>().write_unaligned(ptr.cast::<Packer::IValue>().read_unaligned() + Packer::IValue::from(1));
+            }
+            return id
+        } else {
+            let offset: usize = self.stream.len();
+            self.stream.push(Packer::ID);
+            Packer::pack_value(&mut self.stream, Packer::IValue::from(1));
+            return TypedIndexId { index: offset, typedId: Packer::ID, _id: PhantomData }
+        }
     }
 
     fn at(&self, id: IndexId<'id>) -> u8 {
@@ -180,6 +204,7 @@ impl<'id> ByteStreamCtx<'id> {
 
 pub struct ByteStreamBuilder {
     ctx: ByteStreamCtx<'static>,
+    head: TypedIndexId<'static>,
 }
 
 impl IRBuilderTrait for ByteStreamBuilder {
@@ -187,27 +212,35 @@ impl IRBuilderTrait for ByteStreamBuilder {
 
     #[inline]
     fn new() -> Self {
+        let mut new_ctx = ByteStreamCtx::new();
+        new_ctx.stream.reserve_exact(4096);
+
+        let new_head = new_ctx.alloc::<INone>();
+        new_ctx.alloc::<INone>();
+        new_ctx.alloc::<INone>();
+
         return Self { 
-            ctx: ByteStreamCtx::new(),
+            ctx: new_ctx,
+            head: new_head,
         };
     }
 
     #[inline]
     fn enter_token(&mut self, token: BNToken) -> Result<(), BNError> {
         match token {
-            BNToken::Add => { self.ctx.alloc_packed::<IAdd>(1); },
-            BNToken::Sub => { self.ctx.alloc_packed::<ISub>(1); },
-            BNToken::Right => { self.ctx.alloc_packed::<IRight>(1); },
-            BNToken::Left => { self.ctx.alloc_packed::<ISub>(1); },
-            BNToken::In => { self.ctx.alloc::<IIn>(); },
-            BNToken::Out => { self.ctx.alloc::<IOut>(); },
+            BNToken::Add => { self.head = self.ctx.alloc_or_increment::<IAdd>(self.head); },
+            BNToken::Sub => { self.head = self.ctx.alloc_or_increment::<ISub>(self.head); },
+            BNToken::Right => { self.head = self.ctx.alloc_or_increment::<IRight>(self.head); },
+            BNToken::Left => { self.head = self.ctx.alloc_or_increment::<ISub>(self.head); },
+            BNToken::In => { self.head = self.ctx.alloc::<IIn>(); },
+            BNToken::Out => { self.head = self.ctx.alloc::<IOut>(); },
             BNToken::Loop => {
-                self.ctx.alloc_packed::<ILoop>(1);
+                self.head = self.ctx.alloc_packed::<ILoop>(1);
                 // let value: IValue = 32;
                 // self.ctx.alloc_id_slice(&value.to_ne_bytes(), Inst::Loop(0).discriminant());
             },
             BNToken::EndLoop => {
-                self.ctx.alloc_packed::<IEndLoop>(1);
+                self.head = self.ctx.alloc_packed::<IEndLoop>(1);
                 // let value: IValue = 64;
                 // self.ctx.alloc_id_slice(&value.to_ne_bytes(), Inst::EndLoop(0).discriminant());
             },
@@ -218,7 +251,7 @@ impl IRBuilderTrait for ByteStreamBuilder {
 
     #[inline]
     fn finalize(self) -> Result<Vec<u8>, BNError> {
-        return Ok(self.ctx.stream.into_iter().collect());
+        return Ok(self.ctx.stream.into_iter().skip(3).collect());
     }
 }
 
@@ -230,6 +263,11 @@ mod bnstream_test {
         // let mut stream = ByteStream{ ctx: ByteStreamCtx::new() };
         let mut builder = ByteStreamBuilder::new();
 
+        builder.enter_token(BNToken::Add).unwrap();
+        builder.enter_token(BNToken::Add).unwrap();
+        builder.enter_token(BNToken::Add).unwrap();
+        builder.enter_token(BNToken::Add).unwrap();
+        builder.enter_token(BNToken::Add).unwrap();
         builder.enter_token(BNToken::Add).unwrap();
         builder.enter_token(BNToken::Sub).unwrap();
         builder.enter_token(BNToken::Right).unwrap();
